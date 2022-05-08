0d2ccb7b3e3443a81254b94d5e6faac5330a48bb
indrasatriaxx <indrapanca.xx@gmail.com>
2022-05-08 22:35:44 +0700

    tambah tombol delete dan clear

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -5,19 +5,18 @@ export const MainPage = () => {
   const [TaskInput, setTaskInput] = useState("");
   const [Task, setTask] = useState([]);
 
-
   //ambil data dari local storage
   useEffect(() => {
     //cek ada data atau tidak
     if (localStorage.getItem("localTasks")) {
       //ambil data dari lcoal storage dan rubah data ke JSON
       const TaskStored = JSON.parse(localStorage.getItem("localTasks"));
+
       //data dari local storage dimasukan ke state Task
       setTask(TaskStored);
     }
   }, []);
 
-  
   const addTask = (e) => {
     if (TaskInput) {
       const newTask = { id: new Date().getTime().toString(), title: TaskInput };
@@ -25,6 +24,19 @@ export const MainPage = () => {
       localStorage.setItem("localTasks", JSON.stringify([...Task, newTask]));
       setTaskInput("");
     }
+  };
+
+  // handle delete
+  const handleDelete = (task) => {
+    const deleted = Task.filter((t) => t.id !== task.id);
+    setTask(deleted);
+    localStorage.setItem("localTasks", JSON.stringify(deleted));
+  };
+
+  // handle clear
+  const handleClear = () => {
+    setTask([]);
+    localStorage.removeItem("localTasks");
   };
 
   const showTask = (data) => {
@@ -37,6 +49,7 @@ export const MainPage = () => {
 
   return (
     <div>
+      <h1> To Do App</h1>
       <input
         type="text"
         value={TaskInput}
@@ -54,6 +67,18 @@ export const MainPage = () => {
       </button>
 
       {showTask(Task)}
+
+      {/* Tombol delete */}
+
+      <div className="tbl-delete">
+        <button onClick={() => handleDelete(Task)}>delete</button>
+      </div>
+
+      {/* tombol clear */}
+
+      <div className="tbl-clear">
+        <button onClick={() => handleClear()}>Clear</button>
+      </div>
     </div>
   );
 };
